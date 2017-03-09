9a80a1b8169b808ee1fec7959ef520014c4d7d11
Alexander Boris <index.wow202@gmail.com>
2017-03-09 18:03:06 +0300

    Layout and adaptive slider adventures, catalog list, pagination

--- src/js/internal.js
+++ src/js/internal.js
@@ -1,12 +1,65 @@
 (function($) {
 
-    $('.icon-menu').on('click', function () {
-       $('.mobile-menu').addClass('active');
-       $('body').addClass('no-scroll');
+    var grid = {
+        xs: 480,
+        sm: 768,
+        md: 992,
+        lg: 1200
+    };
+    var elements = {
+        mobileMenu: $('.mobile-menu'),
+        prevArrow:'<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>'
+    };
+    var options = {
+        documentWidth: $(document).width()
+    };
+
+    $(window).resize(function () {
+        options.documentWidth = $(document).width();
+
+        if(options.documentWidth >= grid.sm) {
+            elements.mobileMenu.removeClass('active');
+            $('body').removeClass('no-scroll');
+        }
     });
-    $('.mobile-menu .icon-cross').on('click', function () {
-       $('.mobile-menu').removeClass('active');
-       $('body').removeClass('no-scroll');
+
+    $('.icon-menu').on('click', function () {
+        elements.mobileMenu.addClass('active');
+        if(options.documentWidth < grid.sm) {
+            $('body').addClass('no-scroll');
+        }
+    });
+    elements.mobileMenu.find('.icon-cross').on('click', function () {
+        elements.mobileMenu.removeClass('active');
+        $('body').removeClass('no-scroll');
+    });
+
+    $('.slider-advantage').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: true,
+        prevArrow: elements.prevArrow,
+        nextArrow: elements.nextArrow,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
 })(jQuery);
