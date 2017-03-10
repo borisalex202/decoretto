06d93a4fc2784c9e23e7b4cf854f34d139f5cdbc
Alexander Boris <index.wow202@gmail.com>
2017-03-10 18:15:06 +0300

    Improvements, layout and adaptive filter, catalog and pop-up load images

--- src/js/internal.js
+++ src/js/internal.js
@@ -8,6 +8,7 @@
     };
     var elements = {
         mobileMenu: $('.mobile-menu'),
+        moreBlock: $('.more-block'),
         prevArrow:'<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
         nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>'
     };
@@ -35,6 +36,19 @@
         $('body').removeClass('no-scroll');
     });
 
+    $('.smooth-link').on('click', function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 700);
+                return false;
+            }
+        }
+    });
+
     $('.slider-advantage').slick({
         dots: true,
         arrows: true,
@@ -60,6 +74,50 @@
                 }
             }
         ]
+    });
+
+    $('[data-more]').on('click', function (e) {
+        var el = $(this).attr('href');
+        e.preventDefault();
+
+        $(this).text(function(i, text){
+            return text === 'Показать все' ? 'Скрыть' : 'Показать все';
+        });
+        $(el).slideToggle();
+    });
+
+    $('.filter-toggle').on('click', function () {
+        $('.filter').addClass('active');
+        $('.photo-wallpapers').addClass('active');
+    });
+    $('.filter .icon-cross').on('click', function () {
+        $('.filter').removeClass('active');
+        $('.photo-wallpapers').removeClass('active');
+    });
+
+    $('.catalog-tools').find('.btn').on('click', function (e) {
+       if($(this).hasClass('active')) {
+           return false;
+       } else {
+           e.preventDefault();
+           $('.catalog-tools').find('.btn').removeClass('active');
+           $(this).addClass('active');
+       }
+    });
+    $('[data-toggle="popup"]').on('click', function (e) {
+        var el = $(this).attr('href');
+        e.preventDefault();
+
+        $(el).addClass('in');
+    });
+
+    $(document).mouseup(function (e){
+        var el = $('#load_image');
+        if (!el.is(e.target)
+            && el.has(e.target).length === 0) {
+            el.removeClass('in');
+            $('.catalog-tools').find('.btn').addClass('active').not(':first-child').removeClass('active');
+        }
     });
 
 })(jQuery);
