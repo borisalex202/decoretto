d0a36529803ad5edc6ca848aa4cd6b73fb3f0e6c
Alexander Boris <index.wow202@gmail.com>
2017-03-15 17:14:29 +0300

    Add mask for input phone and improvements

--- src/js/internal.js
+++ src/js/internal.js
@@ -9,10 +9,11 @@
     var elements = {
         mobileMenu: $('.mobile-menu'),
         moreBlock: $('.more-block'),
+        approach: $('.approach'),
         prevArrow:'<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
         nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>',
-        prevArrowText:'<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-pag-left"><use xlink:href="#icon-arrow-pag-left"></use></svg> Предыдущая</button>',
-        nextArrowText: '<button class="slick-arrow slick-next">Следующая <svg class="icon icon-arrow-pag-right"><use xlink:href="#icon-arrow-pag-right"></use></svg></button>'
+        prevArrowText:'<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-pag-left"><use xlink:href="#icon-arrow-pag-left"></use></svg> <span>Предыдущая</span></button>',
+        nextArrowText: '<button class="slick-arrow slick-next"><span>Следующая</span> <svg class="icon icon-arrow-pag-right"><use xlink:href="#icon-arrow-pag-right"></use></svg></button>'
     };
     var options = {
         documentWidth: $(document).width()
@@ -21,12 +22,17 @@
     @@include('./partials/_sliders.js');
     @@include('./partials/_map.js');
 
-    $(window).resize(function () {
+    $(window).on('load resize', function () {
         options.documentWidth = $(document).width();
 
         if(options.documentWidth >= grid.sm) {
             elements.mobileMenu.removeClass('active');
             $('body').removeClass('no-scroll');
+        }
+        if(options.documentWidth >= grid.sm && options.documentWidth < grid.md) {
+            elements.approach.css('background-size', getHeightApproach());
+        } else {
+            elements.approach.css('background-size', 'inherit');
         }
     });
 
@@ -108,4 +114,15 @@
         paramName: "file",
         maxFilesize: 100
     });
+
+    $('input[type="tel"]').mask('+7(999)999-99-99', {
+        placeholder: "+7(___)___-__-__"
+    });
+    
+    function getHeightApproach () {
+        var summaryH = elements.approach.find('.heading').outerHeight() + elements.approach.find('.left').outerHeight() + 130;
+
+        return summaryH;
+    }
+
 })(jQuery);
